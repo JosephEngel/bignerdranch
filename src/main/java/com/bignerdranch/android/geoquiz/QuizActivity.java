583d4b238d1b5c7e28d3f279df28e7dfb2942811
joeyengel26 <joeyengel@zoho.com>
2017-09-21 23:49:23 -0400

    Add files via upload
    Chapter 2 Complete

--- src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -4,25 +4,57 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class QuizActivity extends Activity {
+public class QuizActivity extends Activity { // Creates the subclass QuizActivity
 
-    private Button mTrueButton;
-    private Button mFalseButton;
+    private Button mTrueButton;  // constructor - creating member variable
+    private Button mFalseButton; // constructor - creating member variable
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    private Question [] mQuestionBank = new Question[] {
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
+    };
+
+    private int mCurrentIndex = 0;
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+        int messageResId = 0;
+
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { // Method that's called when an activity (QuizActivity) subclass is created
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-        mTrueButton = (Button) findViewById(R.id.true_button);
-        mTrueButton.setOnClickListener(new View.OnClickListener() {
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);  // Method that sets our variable to the Resource_id true_button (defined in activity_quiz.xml)
+        mTrueButton.setOnClickListener(new View.OnClickListener() {  // Call interface (setOnClickListener) on our object (mTrueButton); syntax = variable.method()
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
-                        R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
             }
         });
 
@@ -30,10 +62,20 @@ public class QuizActivity extends Activity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
-                        R.string.correct_toast,
-                        Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
+
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        updateQuestion();
+
     }
 }
