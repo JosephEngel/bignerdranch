43e443c16d551d0080fb81f27d2383056b119ab2
joeyengel26 <joeyengel@zoho.com>
2017-09-22 01:26:31 -0400

    Add files via upload
    Ch.2 Challenge 3 complete

--- main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,8 +12,8 @@ public class QuizActivity extends Activity { // Creates the subclass QuizActivit
 
     private Button mTrueButton;  // constructor - creating member variable
     private Button mFalseButton; // constructor - creating member variable
-    private Button mNextButton;
-    private Button mPrevButton;
+    private ImageButton mNextButton;
+    private ImageButton mPrevButton;
     private TextView mQuestionTextView;
 
     private Question [] mQuestionBank = new Question[] {
@@ -75,7 +76,7 @@ public class QuizActivity extends Activity { // Creates the subclass QuizActivit
             }
         });
 
-        mPrevButton = (Button) findViewById(R.id.prev_button);
+        mPrevButton = (ImageButton) findViewById(R.id.prev_button);
         mPrevButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -87,7 +88,7 @@ public class QuizActivity extends Activity { // Creates the subclass QuizActivit
             }
         });
 
-        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
