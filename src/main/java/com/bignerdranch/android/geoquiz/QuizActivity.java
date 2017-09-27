c9462331ad0927125f33260a62afd233b2babe05
joeyengel26 <joeyengel@zoho.com>
2017-09-27 00:54:41 -0400

    Add files via upload
    Chapter 3 Complete

--- src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,12 +2,16 @@ package com.bignerdranch.android.geoquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends Activity { // Creates the subclass QuizActivity
+
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
 
     private Button mTrueButton;  // constructor - creating member variable
     private Button mFalseButton; // constructor - creating member variable
@@ -46,6 +50,7 @@ public class QuizActivity extends Activity { // Creates the subclass QuizActivit
     @Override
     protected void onCreate(Bundle savedInstanceState) { // Method that's called when an activity (QuizActivity) subclass is created
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
@@ -75,7 +80,48 @@ public class QuizActivity extends Activity { // Creates the subclass QuizActivit
             }
         });
 
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+
         updateQuestion();
 
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 }
